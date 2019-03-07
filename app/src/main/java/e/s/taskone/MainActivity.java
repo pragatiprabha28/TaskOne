d4e31108cc3932ae3d870c64ee6ed0aa95fd0b54
pragatiprabha28 <pragati.prabha_mca18@gla.ac.in>
2019-03-07 10:53:59 +0530

    init comit

---
+++ app/src/main/java/e/s/taskone/MainActivity.java
@@ -0,0 +1,19 @@
+package e.s.taskone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.)
+    }
+}
